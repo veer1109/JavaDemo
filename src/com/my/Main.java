3ae1e7b0da4cd6bffd0186cbdcf3fb673d51768e
veer1109 <veer.patil@ash-software.com>
2017-12-19 14:39:37 +0530

    Added comments to the document

---
+++ src/com/my/Main.java
@@ -0,0 +1,16 @@
+package com.my;
+
+public class Main {
+    //Line Comments ia added
+    /* Block Comments
+    can be multiple line
+     */
+
+    /** Java Document comment
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+	 System.out.println("Hello World");
+    }
+}
