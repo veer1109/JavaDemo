70870951d691884a04288f430049f3ced96a5588
veer1109 <veer.patil@ash-software.com>
2018-01-06 23:12:02 +0530

    Added code to check multiple URL's

--- src/com/my/CheckWebSiteStatus.java
+++ src/com/my/CheckWebSiteStatus.java
@@ -2,13 +2,24 @@ package com.my;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckWebSiteStatus {
 
+
     public static  void main(String []arg)
     {
-      int status=  CheckWebStatus("https://www.villaplus.com");
-      System.out.println("Website status is:"+ status);
+        List urls = new ArrayList();
+      urls.add("https://www.villaplus.com");
+        urls.add("https://t.villaplus.com");
+        urls.add("https://m.villaplus.com");
+        for (Object u: urls) {
+
+          System.out.println("WEB:"+  u + " Status: "  + CheckWebStatus(u.toString()));
+        }
+
+
     }
     public static int CheckWebStatus(String url)
     {
@@ -19,9 +30,22 @@ public class CheckWebSiteStatus {
             conn.setRequestMethod("GET");
             conn.connect();
             int code = conn.getResponseCode();
-           System.out.println(conn.getResponseMessage());
+            if(code ==200) {
+                System.out.println(conn.getResponseMessage());
+                return code;
+            }
+            else if(code==301)
+            {
+                System.out.println(conn.getResponseMessage());
+                return code;
+            }
+            else if(code==404)
+            {
+                System.out.println(conn.getResponseMessage());
+                return code;
+            }
 
-            return code;
+
         }
         catch (Exception ex)
         {
