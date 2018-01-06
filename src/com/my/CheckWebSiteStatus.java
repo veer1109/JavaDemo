687e55ad9bef7fe5d2fc66c16450ab557b365e44
veer1109 <veer.patil@ash-software.com>
2018-01-06 22:57:01 +0530

    Updated to include method checkwebstatus

--- src/com/my/CheckWebSiteStatus.java
+++ src/com/my/CheckWebSiteStatus.java
@@ -7,23 +7,26 @@ public class CheckWebSiteStatus {
 
     public static  void main(String []arg)
     {
+      int status=  CheckWebStatus("https://www.villaplus.com");
+      System.out.println("Website status is:"+ status);
+    }
+    public static int CheckWebStatus(String url)
+    {
         try {
-            String httpsURL = "https://www.villaplus.com";
-            URL myUrl = new URL(httpsURL);
+            //String httpsURL = "https://www.villaplus.com";
+            URL myUrl = new URL(url);
             HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
-
             conn.setRequestMethod("GET");
-
-
-
             conn.connect();
             int code = conn.getResponseCode();
+           System.out.println(conn.getResponseMessage());
 
-            System.out.println(code);
+            return code;
         }
         catch (Exception ex)
         {
             System.out.println(ex.getMessage());
         }
+        return 0;
     }
 }
